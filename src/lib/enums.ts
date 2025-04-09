9728278f4aa441711a3578b775f301d42389d6f4
Shivam <bishtshivam096@gmail.com>
2025-04-09 22:06:48 +0530

    added subscription list

---
+++ src/lib/enums.ts
@@ -0,0 +1,28 @@
+export enum Status {
+	open = 'open',
+	closed = 'closed',
+}
+
+export enum AttendanceStatus {
+	present = 'present',
+	absent = 'absent',
+}
+
+export enum MembershipTypeEntry {
+	expired = 'expired',
+	start = 'start',
+	upgrade = 'upgrade',
+}
+
+export enum OrderStatus {
+	paid = 'paid',
+	cancelled = 'cancelled',
+	refund = 'refund',
+	pending = 'pending',
+}
+
+export enum PlayerRank {
+	prime = 'prime',
+	elite = 'elite',
+	developing = 'developing',
+}
