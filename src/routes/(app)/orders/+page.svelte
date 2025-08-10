214cbbcdce0dc503282ce3f46d224c3c8ea07478
Shivam <bishtshivam096@gmail.com>
2025-08-10 23:00:48 +0530

    added name and phone in orders

--- src/routes/(app)/orders/+page.svelte
+++ src/routes/(app)/orders/+page.svelte
@@ -96,7 +96,9 @@
 				<p class="m-0"><strong>Discount:</strong> ₹{order.Price.Discount}</p>
 				<p class="m-0"><strong>Sub Total:</strong> ₹{order.Price.Total}</p>
 
-				{#if isLogistics && order.UserInfo?.Address}
+				{#if order.UserInfo?.Address}
+					<p class="mt-3 m-0"><strong>Name:</strong> {order.UserInfo.Name}</p>
+					<p class="m-0"><strong>Phone:</strong> {order.UserInfo.Phone}</p>
 					<p class="mt-3 m-0"><strong>Pickup Address:</strong></p>
 					<p class="m-0">Line 1: {order.UserInfo?.Address?.Line1}</p>
 					<p class="m-0">Line 2: {order.UserInfo?.Address?.Line2}</p>
