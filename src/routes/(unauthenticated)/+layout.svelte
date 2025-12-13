df0628cfeb1b6ef1fc03017752e6c5a5f6709105
Shivam <bishtshivam096@gmail.com>
2025-12-13 17:05:14 +0530

    preloaded image on load

--- src/routes/(unauthenticated)/+layout.svelte
+++ src/routes/(unauthenticated)/+layout.svelte
@@ -1,3 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	// Preload the coming-soon image for all unauthenticated routes
+	onMount(() => {
+		const img = new Image();
+		img.src = '/imgs/coming-soon.png';
+	});
+</script>
+
+<svelte:head>
+	<!-- Preload critical image -->
+	<link rel="preload" as="image" href="/imgs/coming-soon.png" />
+</svelte:head>
+
 <div id="outermost_app_wrap">
 	<div id="main_container">
 		<slot />
